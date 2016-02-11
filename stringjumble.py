3925814e52f73f93a572a4f7ef52a3c3bdeffc57
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-02-11 13:16:56 -0500

    Updated from Brython Server: 2/11/2016 1:16:57 PM

--- stringjumble.py
+++ stringjumble.py
@@ -35,8 +35,8 @@ for y in range(1, b+1) :
     print(string[-y],end="")
 print()
 
-word = list(reversed(sost))
-for word in b :
+word = list(reverse(sost))
+for word in l :
     print(word, end="")
 print("")
 
